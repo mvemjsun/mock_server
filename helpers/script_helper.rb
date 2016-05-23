16de1641fd36a53d7dd1afcff709386f66bc6c65
Puneet Teng <pompyt@yahoo.co.uk>
2016-05-23 21:42:50 +0100

    Process scripts safely

--- helpers/script_helper.rb
+++ helpers/script_helper.rb
@@ -2,20 +2,19 @@ module ApplicationHelper
 
   def process_before_script
     if has_before_script?
-
+      process_script :before
     end
   end
 
   def process_after_script
     if has_after_script?
-
+      process_script :after
     end
   end
 
   def has_before_script?
     row = get_current_request_db_row
     if row && row[:has_before_script]
-      p "Request has before script - #{row[:before_script_name]}"
       return true
     end
   end
@@ -23,13 +22,12 @@ module ApplicationHelper
   def has_after_script?
     row = get_current_request_db_row
     if row && row[:has_after_script]
-      p "Request has before script - #{row[:after_script_name]}"
       return true
     end
   end
 
   def get_current_request_db_row
-    # if !defined? @current_request_db_data
+    if !defined? @current_request_db_data
       @current_request_db_data = nil
       url = request.fullpath.sub!(/^\//, '')
       query = Mockdata.where(mock_request_url: url,
@@ -40,16 +38,34 @@ module ApplicationHelper
       if query.any?
         @current_request_db_data = query.first
       else
-        wild_try = try_wildcard_route_mock_data((request.request_method).upcase,ENV['TEST_ENV'])
+        wild_try = try_wildcard_route_mock_data((request.request_method).upcase, ENV['TEST_ENV'])
         if wild_try.has_key? :error
         else
           @current_request_db_data = Mockdata.where(id: wild_try[:id]).first
         end
       end
-    # else
-    #   p "@current_request_db_data already set - #{@current_request_db_data}"
-    # end
+    end
     return @current_request_db_data
+  end
+
+  def process_script(type)
+    begin
+      case type
+        when :before
+          p "Processed before script #{@current_request_db_data[:before_script_name]}"
+          row = Rubyscript.where(script_name: @current_request_db_data[:before_script_name]).first
+          eval( row.script_body)
+        when :after
+          p "Processed after script #{@current_request_db_data[:after_script_name]}"
+          row = Rubyscript.where(script_name: @current_request_db_data[:after_script_name]).first
+          eval(row.script_body)
+      end
+    rescue => error
+      p '------ SCRIPT ERROR ----------'
+      p error.message
+      p '------------------------------'
+
+    end
   end
 
 end
