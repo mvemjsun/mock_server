1cbddabd36e9543c0c90c63519f72ee20037957d
mvemjsun <pompyt@yahoo.co.uk>
2016-08-24 12:27:35 +0100

    rewrite start script.

--- start-mock.sh
+++ start-mock.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
+echo "Trying to start stop mock server ..."
+
+PID=`lsof -i:9293 | grep ruby | grep -v grep | awk '{print $2}'`
+
+if [[ -z "$PID" ]]
+then
+  echo "No mock server running."
+else
+  echo "Mock server is running with PID $PID. Will kill and restart."
+  kill -9 $PID
+fi
+echo "Starting mock server now"
+sleep 2
 rackup > /dev/null 2>&1 &
+
+if [ $? == '0' ]
+then
+  echo "Mock server started"
+else
+  echo "Failed to start mock server"
+fi
