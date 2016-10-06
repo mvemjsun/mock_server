695b86f2b84ce4c90b2c0996500b59e4b955e7f5
mvemjsun <pompyt@yahoo.co.uk>
2016-10-06 12:39:35 +0100

    script to stop server

---
+++ stop-mock.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "Trying to stop mock server ..."
+
+PID=`lsof -i:9293 | grep ruby | grep -v grep | awk '{print $2}'`
+
+if [[ -z "$PID" ]]
+then
+  echo "No mock server running."
+else
+  echo "Mock server is running with PID $PID. Will kill now."
+  kill -9 $PID
+fi
+
+sleep 3
+
+PID=`lsof -i:9293 | grep ruby | grep -v grep | awk '{print $2}'`
+if [[ -z "$PID" ]]
+then
+  echo "Mock server stopped."
+else
+  echo "Could not stop mock server."
+  exit 1
+fi
