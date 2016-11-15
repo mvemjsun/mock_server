60ccab897c01a26c3d6942027f9cc098ecf3f830
mvemjsun <pompyt@yahoo.co.uk>
2016-11-15 15:42:10 +0000

    Create mock form validations

--- spec/create_mock_data_spec.rb
+++ spec/create_mock_data_spec.rb
@@ -19,4 +19,41 @@ describe 'The mock/create page' do
     expect(page).to have_content 'Only one URL can be active at a time.'
   end
 
+  it 'should not allow a blank mock name' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_request_name: ''})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Mock name can\'t be blank')
+  end
+
+  it 'should not allows an empty http status code' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_http_status: ''})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Mock http status can\'t be blank')
+  end
+
+  it 'should not allow a blank mock url' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_request_url: ''})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Mock request url can\'t be blank')
+  end
+
+  it 'should not an invalid HTTP status code like 900' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_http_status: '900'})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Please enter a valid HTTP code')
+  end
+
+  it 'should not an invalid HTTP status code like abcd' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_http_status: 'abcd'})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Please enter a valid HTTP code')
+  end
+
+  it 'should give a warning if a JSON content type response is not created with a valid JSON body' do
+    TestHelper.create_test_mock TestHelper.mockData({mock_content_type: 'application/json;charset=UTF-8',
+                                                     mock_http_body: 'not a json'})
+    TestHelper.submit_form_to_create_update_mock_data
+    expect(page).to have_content('Response Body is not a Valid JSON')
+  end
+
 end
